8664b0ae9be9feefe09cf5bbdc2637d7947c6b75
MrPointVirgule <paul-louis.hery@insa-lyon.fr>
2016-10-10 17:14:16 +0200

    Fix deploy-gh-pages.sh

--- FrontEnd - React/deploy-gh-pages.sh
+++ FrontEnd - React/deploy-gh-pages.sh
@@ -3,6 +3,8 @@ set -e
 npm run build
 mv dist gh-pages
 git add gh-pages
+git config --global user.email "CirleCI@unfollowninja.fr"
+git config --global user.name "CirleCI"
 git commit -m "Updated gh-pages from master"
 cd ..
 git subtree push --prefix "FrontEnd - React/gh-pages" origin gh-pages
