78f3e1782134be5a611486b7a088f6d110968c05
MrPointVirgule <paul-louis.hery@insa-lyon.fr>
2016-10-10 16:55:14 +0200

    Fix deploy-gh-pages.sh

--- FrontEnd - React/deploy-gh-pages.sh
+++ FrontEnd - React/deploy-gh-pages.sh
@@ -5,4 +5,4 @@ mv dist gh-pages
 git add gh-pages
 git commit -m "Updated gh-pages from master"
 cd ..
-git subtree push --prefix gh-pages origin gh-pages
+git subtree push --prefix "FrontEnd - React/gh-pages" origin gh-pages
